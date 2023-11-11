ad5b16ba52036effa23e32be4398ec8a9dc374eb
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-11 06:06:18 -0600

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), alpinejs(), sitemap()]
+});
