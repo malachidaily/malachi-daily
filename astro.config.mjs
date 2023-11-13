375c2401982359ba5585162b93127b1b6bdf6092
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-13 07:21:37 -0600

    Added Netlify's adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs(), sitemap()],
   output: 'server',
+  adapter: netlify()
 });
