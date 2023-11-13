43e9015310621d60f10b488bfdcb24cd298d5c1a
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-13 07:04:37 -0600

    Added ability to add/embed a new verse

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), sitemap()]
+  integrations: [tailwind(), alpinejs(), sitemap()],
+  output: 'server',
 });
