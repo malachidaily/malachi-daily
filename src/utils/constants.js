3be8bf1fcd4f91dab1833f7209023e0def534917
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-17 07:51:20 -0600

    added constants file for scripture ref

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const SCRIPTURE_TEXT = 'But Jesus told him, “No! The Scriptures say, ‘People do not live by bread alone, but by every word that comes from the mouth of God.’”';
+export const SCRIPTURE_REF = 'Matthew 4:4 NLT';
