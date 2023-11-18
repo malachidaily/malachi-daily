2342b2885a6e1978ed08da1a00333491924d8678
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-18 06:02:13 -0600

    fixed errors and moved up alpine state

--- src/components/react/NativeFeelingDrawer/index.jsx
+++ src/components/react/NativeFeelingDrawer/index.jsx
@@ -5,8 +5,8 @@ function NativeFeelingDrawer({ children }) {
     <Drawer.Root shouldScaleBackground={true}>
       <Drawer.Trigger asChild>
         <button>
-          <svg class="text-gray-600 flex-shrink-0" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+          <svg className="text-gray-600 flex-shrink-0" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
               <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"/>
               <circle cx="12" cy="12" r="3"/>
             </g>
@@ -15,17 +15,14 @@ function NativeFeelingDrawer({ children }) {
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-          <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
+          <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-fit mt-24 fixed bottom-0 left-0 right-0">
             <div className="p-4 bg-white rounded-t-[10px] flex-1">
-              <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
+              <div id="drawer-handle" className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
               <div className="max-w-md mx-auto">
-                <Drawer.Title className="font-medium mb-4">
-                  Unstyled drawer for React.
+                <Drawer.Title className="font-medium text-2xl mb-4">
+                  Settings
                 </Drawer.Title>
                 {children}
-                <p className="text-zinc-600 mb-2">
-                  fart
-                </p>
               </div>
             </div>
             <div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
@@ -40,9 +37,9 @@ function NativeFeelingDrawer({ children }) {
                     fill="none"
                     height="16"
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     viewBox="0 0 24 24"
                     width="16"
                     aria-hidden="true"
@@ -63,9 +60,9 @@ function NativeFeelingDrawer({ children }) {
                     fill="none"
                     height="16"
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     viewBox="0 0 24 24"
                     width="16"
                     aria-hidden="true"
