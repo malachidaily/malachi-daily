81aa47234f5a8ab4d6f22746f269b3f0d30e93e1
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-18 09:18:51 -0600

    moved footer to drawer

--- src/components/react/NativeFeelingDrawer/index.jsx
+++ src/components/react/NativeFeelingDrawer/index.jsx
@@ -15,14 +15,50 @@ function NativeFeelingDrawer({ children }) {
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-          <Drawer.Content className="flex bg-white flex-col rounded-t-[10px] h-fit mt-24 pb-6 fixed bottom-0 left-0 right-0">
-            <div className="p-4 bg-white rounded-t-[10px] flex-1">
+          <Drawer.Content className="flex bg-white flex-col rounded-t-[10px] h-fit mt-24 fixed bottom-0 left-0 right-0">
+            <div className="p-4 pb-6 bg-white rounded-t-[10px] flex-1">
               <div id="drawer-handle" className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
-              <div className="max-w-md mx-auto">
-                <Drawer.Title className="font-medium text-2xl mb-4">
+              <div className="max-w-md mx-auto flex flex-col gap-4">
+                <Drawer.Title className="font-medium text-2xl">
                   Settings
                 </Drawer.Title>
                 {children}
+                <ul class="w-full flex justify-center gap-6 flex-wrap text-gray-600 p-4 bg-gray-100 rounded-lg">
+                <li>
+									<a 
+										class="link whitespace-nowrap text-sm"
+										x-data={`{
+											createEmailLink() {
+												// Set up the email link for getting help
+												const helpEmailLink = new URL('mailto:cameronandrewpak+malachidaily@gmail.com');
+												helpEmailLink.searchParams.set('cc', 'hello@malachidaily.com');
+												helpEmailLink.searchParams.set('subject', encodeURIComponent('Malachi Daily Help Request'));
+												let emailBody = '';
+												emailBody += 'I want help with the following:\\n\\n';
+												emailBody += '{Write about what you need help with here}\\n\\n';
+												emailBody += '—Below is some necessary data to help debug any issues:—'
+												emailBody += '\\n• Scripture Ref: ' + scriptureRef;
+												emailBody += '\\n• Scripture: ' + scriptureTextOriginal;
+												emailBody += '\\n• User Agent: ' + window.navigator.userAgent;
+												helpEmailLink.searchParams.set('body', encodeURIComponent(emailBody));
+												return decodeURIComponent(helpEmailLink.href)
+											},
+											emailLink: ''
+										}`}
+										x-init="emailLink = createEmailLink()"
+										x-bind:href="emailLink"
+										target="_blank"
+									>
+										Need help?
+									</a>
+                </li>
+                <li>
+                  <a class="link whitespace-nowrap text-sm" href="mailto:hello@malachidaily.com" target="_blank">Contact Us</a>
+                </li>
+                <li>
+                  <a class="link whitespace-nowrap text-sm" href="https://malachidaily.com" target="_blank">Visit Main Website</a>
+                </li>
+              </ul>
               </div>
             </div>
           </Drawer.Content>
