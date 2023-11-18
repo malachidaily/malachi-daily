579683fc8d4d0fdc71120f274b1330d6d414e08d
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-18 06:18:44 -0600

    Add an option to randomize the words and not be in order Malachi Daily

--- src/components/react/NativeFeelingDrawer/index.jsx
+++ src/components/react/NativeFeelingDrawer/index.jsx
@@ -15,7 +15,7 @@ function NativeFeelingDrawer({ children }) {
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-          <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-fit mt-24 fixed bottom-0 left-0 right-0">
+          <Drawer.Content className="flex bg-white flex-col rounded-t-[10px] h-fit mt-24 pb-6 fixed bottom-0 left-0 right-0">
             <div className="p-4 bg-white rounded-t-[10px] flex-1">
               <div id="drawer-handle" className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
               <div className="max-w-md mx-auto">
@@ -23,56 +23,6 @@ function NativeFeelingDrawer({ children }) {
                   Settings
                 </Drawer.Title>
                 {children}
-              </div>
-            </div>
-            <div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
-              <div className="flex gap-6 justify-end max-w-md mx-auto">
-                <a
-                  className="text-xs text-zinc-600 flex items-center gap-0.25"
-                  href="https://github.com/emilkowalski/vaul"
-                  target="_blank"
-                >
-                  GitHub
-                  <svg
-                    fill="none"
-                    height="16"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    width="16"
-                    aria-hidden="true"
-                    className="w-3 h-3 ml-1"
-                  >
-                    <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
-                    <path d="M15 3h6v6"></path>
-                    <path d="M10 14L21 3"></path>
-                  </svg>
-                </a>
-                <a
-                  className="text-xs text-zinc-600 flex items-center gap-0.25"
-                  href="https://twitter.com/emilkowalski_"
-                  target="_blank"
-                >
-                  Twitter
-                  <svg
-                    fill="none"
-                    height="16"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    width="16"
-                    aria-hidden="true"
-                    className="w-3 h-3 ml-1"
-                  >
-                    <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
-                    <path d="M15 3h6v6"></path>
-                    <path d="M10 14L21 3"></path>
-                  </svg>
-                </a>
               </div>
             </div>
           </Drawer.Content>
