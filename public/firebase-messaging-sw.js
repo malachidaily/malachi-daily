ae516df8a4ebdf7c7764596b9372a3e5e6e34b6a
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-19 15:37:21 -0600

    got single notification working!

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -10,13 +10,9 @@ const firebaseConfig = {
     appId: "1:44879717629:web:c06413d20d281de6c24a94",
     measurementId: "G-S0N4P2MHKX"
 };
-
 firebase.initializeApp(firebaseConfig);
 
 const isSupported = firebase.messaging.isSupported();
 if (isSupported) {
     const messaging = firebase.messaging();
-    messaging.onBackgroundMessage(({ notification: { title, body, image } }) => {
-        self.registration.showNotification(title, { body, icon: image || '/logo.png' });
-    });
 }
--- src/messaging_init_in_sw.js
+++ src/messaging_init_in_sw.js
@@ -1,20 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getMessaging, getToken } from "firebase/messaging";
+import firebase from "firebase/compat/app";
+import "firebase/compat/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAB5bk5DBxwgUibsYBYz2_NGk0OaBFhP_4",
-  authDomain: "malachi-daily.firebaseapp.com",
-  projectId: "malachi-daily",
-  storageBucket: "malachi-daily.appspot.com",
-  messagingSenderId: "44879717629",
-  appId: "1:44879717629:web:c06413d20d281de6c24a94",
-  measurementId: "G-S0N4P2MHKX"
+    apiKey: "AIzaSyAB5bk5DBxwgUibsYBYz2_NGk0OaBFhP_4",
+    authDomain: "malachi-daily.firebaseapp.com",
+    projectId: "malachi-daily",
+    storageBucket: "malachi-daily.appspot.com",
+    messagingSenderId: "44879717629",
+    appId: "1:44879717629:web:c06413d20d281de6c24a94",
+    measurementId: "G-S0N4P2MHKX"
 };
+firebase.initializeApp(firebaseConfig);
+export const messaging = firebase.messaging();
 
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const messaging = getMessaging(app);
+export async function getUserFirebaseNotificationToken() {
+    const currentToken = await messaging.getToken({
+        vapidKey: "BCWhyz_ReqLr3lA_dUjrtyHAEJ-LnNPoI-zOdutLHnKnpb9LYVbbox13YlovUIadeyMfq7RWE3fUz0sVfEGokqA",
+    });
+    return currentToken
+}
+
+getUserFirebaseNotificationToken()
 
 /**
  * Requests permission for notifications and returns the current token.
@@ -27,11 +33,6 @@ export async function requestNotificationPermission() {
     if (permission === 'granted') {
         // User can receive messages
         console.log('Notification permissions granted!')
-
-        // Now, tell Firebase that we want to get notification updates.
-        currentToken = await getToken(messaging, {
-            vapidKey: "BCWhyz_ReqLr3lA_dUjrtyHAEJ-LnNPoI-zOdutLHnKnpb9LYVbbox13YlovUIadeyMfq7RWE3fUz0sVfEGokqA",
-        });
     } else if (permission === 'denied') {
         // Permission has been denied
         console.log('Notification permission has been denied.');
