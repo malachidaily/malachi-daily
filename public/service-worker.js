c1b4c0ec1dfc0efea2d62867a0f835261a8e1a68
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-20 05:14:14 -0600

    updated service worker to refresh page once a day

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2.2.5';
+const PRECACHE = 'precache-v2.3';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -65,18 +65,25 @@ self.addEventListener('fetch', event => {
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+        // Check if it's time to reload the page. I want to reload once a day
+        // to get fresh content.
+        const currentHours = new Date().getHours();
+        const reloadWindowStart = 0; // Reload at midnight
+        const reloadWindowEnd = 24; // Reload before midnight
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
+        const isWithinReloadWindow = Boolean(currentHours >= reloadWindowStart && currentHours < reloadWindowEnd);
+        if (isWithinReloadWindow || !cachedResponse) {
+          return caches.open(RUNTIME).then(cache => {
+            return fetch(event.request).then(response => {
+              // Put a copy of the response in the runtime cache.
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
             });
           });
-        });
+        }
+
+        return cachedResponse;
       })
     );
   }
