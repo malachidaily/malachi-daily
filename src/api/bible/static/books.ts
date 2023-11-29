0a014501653bdc367d9974357785b336adba8673
Cameron Pak <cameronandrewpak@gmail.com>
2023-11-29 08:44:24 -0600

    got books being fetched :)

--- src/api/bible/static/books.ts
+++ src/api/bible/static/books.ts
@@ -27,7 +27,7 @@ export const bibleVersionData: { [key in bibleVersion]: { readableName: string, 
 
 export type BookName = "Genesis" | "Exodus" | "Leviticus" | "Numbers" | "Deuteronomy" | "Joshua" | "Judges" | "Ruth" | "1 Samuel" | "2 Samuel" | "1 Kings" | "2 Kings" | "1 Chronicles" | "2 Chronicles" | "Ezra" | "Nehemiah" | "Esther" | "Job" | "Psalms" | "Proverbs" | "Ecclesiastes" | "Song of Solomon" | "Isaiah" | "Jeremiah" | "Lamentations" | "Ezekiel" | "Daniel" | "Hosea" | "Joel" | "Amos" | "Obadiah" | "Jonah" | "Micah" | "Nahum" | "Habakkuk" | "Zephaniah" | "Haggai" | "Zechariah" | "Malachi" | "Matthew" | "Mark" | "Luke" | "John" | "Acts" | "Romans" | "1 Corinthians" | "2 Corinthians" | "Galatians" | "Ephesians" | "Philippians" | "Colossians" | "1 Thessalonians" | "2 Thessalonians" | "1 Timothy" | "2 Timothy" | "Titus" | "Philemon" | "Hebrews" | "James" | "1 Peter" | "2 Peter" | "1 John" | "2 John" | "3 John" | "Jude" | "Revelation" | "Psalm"
 
-type Book = {
+export type Book = {
     bookid: number;
     // This is the chapter name of the book, such as "Genesis"
     name: BookName;
