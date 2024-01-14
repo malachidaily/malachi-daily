d9637247e99d8a36372ffc80f231c6e1269a68d6
Cameron Pak <cameronandrewpak@gmail.com>
2024-01-14 08:30:47 -0600

    fixed nav drawer

--- src/components/react/NativeFeelingDrawer/index.tsx
+++ src/components/react/NativeFeelingDrawer/index.tsx
@@ -20,7 +20,7 @@ function NativeFeelingDrawer({ children } : { children: React.ReactNode }) {
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-          <Drawer.Content className="outline-0 flex bg-white dark:bg-gray-800 flex-col rounded-t-[10px] h-fit mt-24 fixed bottom-0 left-0 right-0 top-0">
+          <Drawer.Content className="outline-0 flex bg-white dark:bg-gray-800 flex-col rounded-t-[10px] h-full mt-24 fixed bottom-0 left-0 right-0 top-0">
             <div className="bg-white dark:bg-gray-800 p-4 safe-area-bottom-padding-extra rounded-t-[10px] flex-1">
               <div id="drawer-handle" className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 dark:bg-gray-600 mb-8 cursor-grab active:cursor-grabbing" />
               <div className="max-w-md mx-auto flex flex-col gap-4">
